a0acc6dea04af6f995a41f8625df4fa9fc028013
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-21 19:25:08 +0530

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,1 +1,1 @@
-sudo suo && apt update && apt install python3 python3-dev python3-venv && wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
+sudo su && apt update && apt install python3 python3-dev python3-venv && wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
